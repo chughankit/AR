ca33f8a3919ea5adec5ecce87b0a46daa4c0707d
chughankit <ankit.chugh@outlook.com>
2025-10-18 19:08:34 +0100

    change 3 again

--- app/src/main/java/com/example/holoai/ar/HoloArFragment.kt
+++ app/src/main/java/com/example/holoai/ar/HoloArFragment.kt
@@ -3,6 +3,7 @@ package com.example.holoai.ar
 import com.google.ar.core.Config
 import com.google.ar.core.Session
 import com.google.ar.sceneform.ux.ArFragment
+import com.gorisse.thomas.sceneform.light.LightEstimationConfig
 
 /**
  * Custom AR fragment that downgrades light estimation to avoid calling
@@ -11,6 +12,9 @@ import com.google.ar.sceneform.ux.ArFragment
 class HoloArFragment : ArFragment() {
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
+        setOnViewCreatedListener { arSceneView ->
+            arSceneView.lightEstimationConfig = LightEstimationConfig.AMBIENT_INTENSITY
+        }
         setOnSessionConfigurationListener { _, config ->
             config.lightEstimationMode = Config.LightEstimationMode.AMBIENT_INTENSITY
         }
