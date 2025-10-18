a55c6e0ca9a4f6c534a9ef1da12e00fa48dc5373
chughankit <ankit.chugh@outlook.com>
2025-10-18 18:51:28 +0100

    HoloArFragment.kt

--- app/src/main/java/com/example/holoai/ar/HoloArFragment.kt
+++ app/src/main/java/com/example/holoai/ar/HoloArFragment.kt
@@ -1,36 +1,19 @@
 package com.example.holoai.ar
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.google.ar.core.Config
 import com.google.ar.core.Session
 import com.google.ar.sceneform.ux.ArFragment
-import com.gorisse.thomas.sceneform.light.LightEstimationConfig
 
 /**
  * Custom AR fragment that downgrades light estimation to avoid calling
  * LightEstimate#acquireEnvironmentalHdrCubeMap() on devices with older ARCore builds.
  */
 class HoloArFragment : ArFragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+        super.onCreate(savedInstanceState)
         setOnSessionConfigurationListener { _, config ->
             config.lightEstimationMode = Config.LightEstimationMode.AMBIENT_INTENSITY
         }
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        arSceneView.lightEstimationConfig = LightEstimationConfig(
-            mode = Config.LightEstimationMode.AMBIENT_INTENSITY
-        )
     }
 
     override fun onCreateSessionConfig(session: Session): Config {
